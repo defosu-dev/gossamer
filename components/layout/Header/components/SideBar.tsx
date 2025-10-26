4c2d27e36f78bc4d1d4f51a096af4308019c551b
defosu-dev <defosu.33@gmail.com>
2025-10-26 20:35:01 +0300

    feat: enhance SideBar component with user authentication handling and improved layout

--- components/layout/Header/components/SideBar.tsx
+++ components/layout/Header/components/SideBar.tsx
@@ -1,9 +1,12 @@
 import React from "react";
-import { Search } from "lucide-react";
+import { Search, UserRound } from "lucide-react";
 import { ImageWithFallback } from "@/components/common/ImageWithFallback";
 import Cart from "./Cart/Cart";
+import { cn } from "@/utils/cn";
+import Link from "next/link";
 
 const SideBar = () => {
+  const isAuth = false;
   return (
     <div className="flex items-center gap-6">
       <button className="w-9 h-9 flex justify-center items-center rounded-full border border-neutral-300">
@@ -12,8 +15,20 @@ const SideBar = () => {
 
       <Cart />
 
-      <div className="w-9 h-9 overflow-hidden rounded-full border border-neutral-300">
-        <ImageWithFallback src="https://via.placeholder.com/36" alt="User Avatar" />
+      <div className={cn(
+        "w-9 h-9 overflow-hidden rounded-full border border-neutral-300",
+        "flex justify-center items-center",
+      )}>
+        {isAuth ?
+          <ImageWithFallback src="https://via.placeholder.com/36" alt="User Avatar" />
+          :
+          <Link href="/auth/sign-in" className={cn(
+            "h-full w-full",
+            "flex justify-center items-center",
+          )}>
+            <UserRound className="size-4.5 text-gray-700" />
+          </Link>
+        }
       </div>
     </div>
   );
